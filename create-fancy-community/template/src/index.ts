3e4ca1b299cc23d6c1a26db89642c1690e41f742
João Vitor Klabacher <41272117+klabacher@users.noreply.github.com>
2026-09-08 16:37:23 -0300

    fix(dx): export scaffold runtime module

--- create-fancy-community/template/src/index.ts
+++ create-fancy-community/template/src/index.ts
@@ -1,11 +1,2 @@
-import type { ModuleManifest } from '@fancydashboard/sdk/plugins/types';
-import { Widget } from './module/Widget';
-
-const moduleEntry: ModuleManifest = {
-  id: '@fancydashboard/pack-module-{{pluginName}}',
-  widgets: {
-    '{{pluginName}}': Widget
-  }
-};
-
-export default moduleEntry;
+export { default } from "./module";
+export { manifest } from "./manifest";
